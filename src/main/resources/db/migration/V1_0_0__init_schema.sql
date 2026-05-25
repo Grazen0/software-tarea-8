86cb1e03599751816f583dbcd6e7a4b51967970e
José Daniel Grayson <josedanielgrayson@proton.me>
2026-05-25 13:49:36 -0500

    feat: use default values for orders.id and orders.created_at

--- src/main/resources/db/migration/V1_0_0__init_schema.sql
+++ src/main/resources/db/migration/V1_0_0__init_schema.sql
@@ -1,6 +1,6 @@
 create table orders (
-    id uuid primary key,
+    id uuid primary key default uuidv7(),
     client_id uuid not null,
     restaurant_code varchar not null,
-    created_at timestamptz not null
+    created_at timestamptz not null default current_timestamp
 );
